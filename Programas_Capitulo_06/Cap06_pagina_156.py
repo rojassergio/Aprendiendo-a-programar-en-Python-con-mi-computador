1a8186c4e08a8916c7933af7163b0d29dec27762
Sergio Rojas <rr_sergio@yahoo.com>
2016-04-23 10:03:49 -0430

    Se incluye capitulo 6: 23 abril 2016

---
+++ Programas_Capitulo_06/Cap06_pagina_156.py
@@ -0,0 +1,31 @@
+'''
+@author: Sergio Rojas
+@contact: rr.sergio@gmail.com
+--------------------------
+Contenido bajo
+ Atribución-NoComercial-CompartirIgual 3.0 Venezuela (CC BY-NC-SA 3.0 VE)
+ http://creativecommons.org/licenses/by-nc-sa/3.0/ve/
+
+Creado en abril 23, 2016
+'''
+
+#In [12]: 
+s = 'este es una, cadena de caracteres. Vamos, es una secuencia'
+
+#In [13]: 
+print(s.split())
+
+#In [14]: 
+print(s.split(','))
+
+#In [15]: 
+print(s.split('.'))
+
+#In [16]: 
+print(s.split(' ',1))
+
+#In [17]: 
+print(s.split('es'))
+
+#In [18]: 
+
